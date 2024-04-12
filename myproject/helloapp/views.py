a73ad42c5add4546441c0ed438e0b4a97e0642c4
YusufTechvitis <yusuf.shatila@techvitis.com>
2024-04-12 14:12:59 +0300

    Search bar

--- myproject/helloapp/views.py
+++ myproject/helloapp/views.py
@@ -3,17 +3,12 @@ from django.shortcuts import render
 import requests
 
 def home(request):
-    try:
-        # Fetch news data from the API
-        api_key = 'fbc50f8083bb4dada1367e8345947f14'
-        url = 'https://newsapi.org/v2/everything?q=tesla&from=2024-03-12&sortBy=publishedAt&apiKey=' + api_key
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an error for non-200 status codes
-        data = response.json()
-        articles = data.get('articles', [])  # Extract articles from data
-    except requests.RequestException as e:
-        # Handle API request error
-        articles = []  # Empty list of articles
-        error_message = str(e)
-    
-    return render(request, 'hello.html', {'articles': articles})
+    query = request.GET.get('query')  # Get the search query from the request
+    api_key = 'fbc50f8083bb4dada1367e8345947f14'
+    url = f'https://newsapi.org/v2/everything?q={query}&from=2024-03-12&sortBy=publishedAt&apiKey={api_key}'
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    articles = data.get('articles', [])
+
+    return render(request, 'hello.html', {'articles': articles, 'query': query})
