fcbf4bd0fa18d33e5719e4fdc8de939512369fe6
YusufTechvitis <yusuf.shatila@techvitis.com>
2024-04-12 17:44:59 +0300

    fwwf

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -4,6 +4,8 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-_8@^wm7b+$_7ec(_vxlb5e7$dgxu69+@++%2w9v*n51n^c!b@f'
+# Define the root directory for collecting static files
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEBUG = True
 
@@ -75,3 +77,4 @@ STATICFILES_DIRS = [
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
