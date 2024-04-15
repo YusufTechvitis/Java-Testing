117844c850f15796b8527d123e2fa9a48c999112
YusufTechvitis <yusuf.shatila@techvitis.com>
2024-04-15 08:43:40 +0300

    api

--- myproject/helloapp/views.py
+++ myproject/helloapp/views.py
@@ -5,7 +5,7 @@ import requests
 def home(request):
     query = request.GET.get('query')  # Get the search query from the request
     api_key = 'fbc50f8083bb4dada1367e8345947f14'
-    url = f'https://newsapi.org/v2/everything?q={query}&from=2024-03-12&sortBy=publishedAt&apiKey={api_key}'
+    url = f'https://newsapi.org/v2/everything?q={query}&from=2024-03-15&sortBy=publishedAt&apiKey={api_key}'
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
